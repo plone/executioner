2b33176af3bf9406a3da7942ac554f3068d936c6
Mathilde <mathilde@onna.com>
2018-07-20 20:20:21 +0200

    styling add form

--- src/app/views/add.ts
+++ src/app/views/add.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 import { AddView } from '@plone/restapi-angular';
 
 @Component({
-  selector: 'generic-add-view',
-  templateUrl: './add.html',
+    selector: 'generic-add-view',
+    templateUrl: './add.html',
 })
-export class GenericAddView extends AddView {}
+export class GenericAddView extends AddView {
+    onSave(model: any) {
+        model['@type'] = this.type;
+        this.services.resource.create(this.path, model).subscribe((res: any) => {
+            this.services.traverser.traverse(this.path + '/' + res['id']);
+        });
+    }
+}
