df107ea397316ae9da43dd9d0a88f8967a799210
Eric Brehault <ebrehault@gmail.com>
2018-07-21 13:16:49 +0200

    toolbar and associated actions

--- src/app/components/toolbar.component.ts
+++ src/app/components/toolbar.component.ts
@@ -1,6 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Services } from '@plone/restapi-angular';
 
 @Component({
     selector: 'g-toolbar',
+    templateUrl: './toolbar.component.html',
+    styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+    @Input() context: any;
+
+    constructor(private services: Services) {}
+
+    delete() {
+        if (confirm('Delete ' + this.context.title)) {
+            const parent = this.context.parent['@id'] || '/';
+            this.services.resource.delete(this.context['@id']).subscribe(() => {
+                this.services.traverser.traverse(parent);
+            });
+        }
+    }
+}
